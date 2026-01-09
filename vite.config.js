329009a7eb307741efafc8b5bfa8896ba65f8ff9
Dhruv Tripathi <dhruvjb12@gmail.com>
2026-01-09 06:32:52 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-refresh'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/fullstack-portfolio/', // This MUST match your repository name
 })
